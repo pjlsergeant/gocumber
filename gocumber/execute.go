dc6e0022f7f16fe8a1bb454d223755289fa35972
Peter Sergeant <pete@clueball.com>
2015-02-14 16:16:18 +0700

    Terrifying hack around testing

--- gocumber/execute.go
+++ gocumber/execute.go
@@ -2,6 +2,9 @@ package gocumber
 
 import "testing"
 import "fmt"
+import "io"
+import "bytes"
+import "os"
 
 const (
 	passed = iota
@@ -12,16 +15,10 @@ const (
 type Executor struct {
 	StepMatcher *StepMatcher
 	Feature     Feature
-	Stash       map[string]interface{}
 	T           *testing.T
 }
 
 func (e *Executor) RunScenario(sc Scenario) int {
-
-	// Check we already have a stash
-	if e.Stash == nil {
-		e.Stash = make(map[string]interface{})
-	}
 
 	// Use the global stepmatcher if we don't have one injected
 	if e.StepMatcher == nil {
@@ -32,9 +29,22 @@ func (e *Executor) RunScenario(sc Scenario) int {
 	shortCircuit := false
 
 	for _, step := range sc.Steps {
-		stepStatus := e.RunStep(step, shortCircuit)
+		stepStatus, output := e.RunStep(step, shortCircuit)
 
-		fmt.Printf("Step [%s] returning %i\n", step.Text, stepStatus)
+		fmt.Printf("Step [%s] ", step.Text)
+
+		switch stepStatus {
+		case passed:
+			fmt.Println("Passed")
+		case failed:
+			fmt.Println("Failed")
+		case pending:
+			fmt.Println("Pending")
+		}
+
+		if stepStatus == failed {
+			fmt.Printf("Error: [%s]\n", output)
+		}
 
 		if scenarioStatus == passed {
 			if stepStatus == pending {
@@ -52,34 +62,79 @@ func (e *Executor) RunScenario(sc Scenario) int {
 	return scenarioStatus
 }
 
-func (e Executor) RunStep(s Step, skip bool) int {
+func makeInternalTest(sc *StepContext, f func(StepContext)) func(*testing.T) {
+	return func(t *testing.T) {
+		sc.T = t
+		f(*sc)
+	}
+}
 
-	// New testing context
-	t := testing.T{}
+var fakeMatchStep = func(string, string) (bool, error) {
+	return true, nil
+}
+
+// You throw the switch and the hideous creatures awakes, rises, and lurches
+// forward. You're simultaneously elated and terrified that something so
+// unnatural could work at all. When you realized what you've unleashed - the
+// pure immorality of it - your creation reaches out with its bloodied, mangled
+// arms and strangles you.
+
+func testSandbox(name string, sc *StepContext, testFunction func(StepContext)) (bool, string) {
+	// Wrap the returned test function to make it accept only testing.T
+	wrapped := makeInternalTest(sc, testFunction)
+
+	// Stick it in to testing.InternalTest format
+	internalTest := testing.InternalTest{
+		Name: name,
+		F:    wrapped,
+	}
+
+	fakeM := testing.MainStart(fakeMatchStep, []testing.InternalTest{internalTest}, []testing.InternalBenchmark{}, []testing.InternalExample{})
+
+	// Steal STDOUT
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	// Throw the switch!
+	osReturnCode := fakeM.Run()
+
+	// Get the output and return stdout
+	outC := make(chan string)
+	// copy the output in a separate goroutine so printing can't block indefinitely
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	// back to normal state
+	w.Close()
+	os.Stdout = old // restoring the real stdout
+	out := <-outC
+
+	return (osReturnCode == 0), out
+}
+
+func (e Executor) RunStep(s Step, skip bool) (int, string) {
 
 	// Build the step context
 	sc := StepContext{
-		Stash: e.Stash,
-		Step:  s,
-		T:     &t,
+		Step: s,
 	}
 
 	// Can we find a matching step definition
 	sd := e.StepMatcher.Match(&sc)
 
-	status := passed
-
 	if sd != nil {
-		t.Logf("Executing for %s", s.Text)
-		sd(sc)
-		if t.Failed() {
-			status = failed
+		success, output := testSandbox(s.Text, &sc, sd)
+
+		if success {
+			return passed, output
 		} else {
-			status = passed
+			return failed, output
 		}
 	} else {
-		status = pending
+		return pending, ""
 	}
-
-	return status
 }
